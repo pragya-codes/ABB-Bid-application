94461af24a949e2a6f5a9104c394b95b14a8b2ab
Pragya <pragya21offc@gmail.com>
2024-10-20 22:06:18 +0530

    Model Schema generation for Auction-Item, Bids, User etc, using Mongoose ODM

---
+++ BACKEND/models/Bid.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define the Bid Schema
+const bidSchema = new mongoose.Schema({
+  auctionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Auction', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  bidAmount: { type: Number, required: true },
+  bidTime: { type: Date, default: Date.now }
+});
+
+const Bid = mongoose.model('Bid', bidSchema);
+module.exports = Bid;
---
+++ BACKEND/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Define the User Schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
