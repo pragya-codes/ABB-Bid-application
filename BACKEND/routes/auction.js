cb5051aac8ed72621adf2b707911dfbc3039a783
Pragya <pragya21offc@gmail.com>
2024-10-20 22:04:23 +0530

    auction route to fetch auction item details, management etc

---
+++ BACKEND/routes/auction.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const Auction = require('../models/Auction');
+const authMiddleware = require('../middleware/authmiddleware');
+const router = express.Router();
+
+// Create a new auction
+router.post('/create', authMiddleware, async (req, res) => {
+      
+      const { title, image, description, startingBid, currentBid, endDate } = req.body;
+  
+  try {
+    const auction = new Auction({
+      title,
+      image,
+      description,
+      startingBid,
+      currentBid,
+      endDate : new Date(endDate),
+     
+    });
+
+    await auction.save();
+    res.status(201).json(auction);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to create auction' });
+  }
+});
+
+//Get all auctions
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    // Fetch all auctions from the Auction model
+    const auctions = await Auction.find();
+    
+    // Send the fetched auctions as the response
+    res.status(200).json(auctions);
+  } catch (error) {
+    // Log and handle any errors that occur during the fetch
+    console.error('Error fetching auctions:', error.message);
+    res.status(500).json({ error: 'Failed to fetch auctions', details: error.message });
+  }
+});
+
+module.exports = router;
+
+//imagelinkformat -   https://drive.google.com/uc?export=view&id=1ftbDkm2OLVWvC7p204UjOxaz8fr1KNM8
