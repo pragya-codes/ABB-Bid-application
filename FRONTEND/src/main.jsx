26cadac430ea30def6930129720f9a1e02574ea4
Pragya <pragya21offc@gmail.com>
2024-10-20 22:26:27 +0530

    main.jsx is the entry point in Frontend. It also contains the routes to different pages.

---
+++ FRONTEND/src/main.jsx
@@ -0,0 +1,55 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import GenixAuctionsHomePage from './Pages/HomePage/GenixAutionsHomePage.jsx';
+import LoginPage from './Pages/LoginPage/LoginPage.jsx';
+import SignUpPage from './Pages/SignUpPage/SignUpPage.jsx';
+import SignUpSuccess from './Pages/SignUpPage/SignUpSuccess.jsx';
+import ExplorePage from './Pages/HomePage/ExplorePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  
+    children: [
+      {
+        index: true,
+        element: <GenixAuctionsHomePage />,
+       
+      },
+      {
+        path: "home",
+        element: <GenixAuctionsHomePage />,
+       
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "signupsuccess",
+        element: <SignUpSuccess />,
+      },
+      {
+        path: "explorepage",
+        element: <ExplorePage />,
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
